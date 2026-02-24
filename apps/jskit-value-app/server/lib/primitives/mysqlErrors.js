6a32bdc971ab10ba3a1b00360369a1151749a03b
Tony Mobily <tonymobily@gmail.com>
2026-02-24 10:42:53 +0800

    remove migrated value app server lib primitives

--- apps/jskit-value-app/server/lib/primitives/mysqlErrors.js
+++
@@ -1,9 +0,0 @@
-function isMysqlDuplicateEntryError(error) {
-  if (!error) {
-    return false;
-  }
-
-  return String(error.code || "") === "ER_DUP_ENTRY";
-}
-
-export { isMysqlDuplicateEntryError };
