5249aa3668a9c9eaab366da56f849e0368b71a83
Tony Mobily <tonymobily@gmail.com>
2026-02-26 14:03:06 +0800

    console: remove unused surface resolver from controller wrapper

--- apps/jskit-value-app/server/modules/console/controller.js
+++ apps/jskit-value-app/server/modules/console/controller.js
@@ -1,11 +1,7 @@
 import { createController as createConsoleAdapterController } from "@jskit-ai/console-fastify-adapter";
-import { resolveSurfaceFromPathname } from "../../../shared/surfacePaths.js";
 
 function createController(options = {}) {
-  return createConsoleAdapterController({
-    ...(options && typeof options === "object" ? options : {}),
-    resolveSurfaceFromPathname
-  });
+  return createConsoleAdapterController(options && typeof options === "object" ? options : {});
 }
 
 export { createController };
