7b3b4ce94f48ee6362c1afe4b3efb4cee1bbb409
Tony Mobily <tonymobily@gmail.com>
2026-03-25 13:42:02 +0800

    feat: add ui-generator package and CLI scaffolding support

---
+++ packages/ui-generator/templates/src/pages/admin/ui-generator/[recordId]/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <View${option:namespace|singular|pascal}Element />
+</template>
+
+<script setup>
+import View${option:namespace|singular|pascal}Element from "../View${option:namespace|singular|pascal}Element.vue";
+</script>
---
+++ packages/ui-generator/templates/src/pages/admin/ui-generator/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <List${option:namespace|plural|pascal}Element />
+</template>
+
+<script setup>
+import List${option:namespace|plural|pascal}Element from "./List${option:namespace|plural|pascal}Element.vue";
+</script>
