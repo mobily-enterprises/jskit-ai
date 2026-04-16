19cc124c8a8d0c262ec76327e1bb4c5ca202e2cb
Tony Mobily <tonymobily@gmail.com>
2026-04-17 00:54:46 +0800

    Track VitePress theme components

---
+++ docs/.vitepress/theme/components/DocsInDepth.vue
@@ -0,0 +1,37 @@
+<template>
+  <aside :class="['docs-in-depth', { 'is-open': open }]" :style="{ '--docs-in-depth-preview-height': previewHeight }">
+    <button
+      class="docs-in-depth__toggle"
+      type="button"
+      :aria-expanded="open ? 'true' : 'false'"
+      @click="open = !open"
+    >
+      <span class="docs-in-depth__title">{{ title }}</span>
+      <span class="docs-in-depth__state">{{ open ? "Hide details" : "Show details" }}</span>
+      <span class="docs-in-depth__chevron" aria-hidden="true">⌄</span>
+    </button>
+
+    <div class="docs-in-depth__frame">
+      <div class="docs-in-depth__content">
+        <slot />
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  title: {
+    type: String,
+    default: "In depth"
+  },
+  previewHeight: {
+    type: String,
+    default: "14rem"
+  }
+});
+
+const open = ref(false);
+</script>
